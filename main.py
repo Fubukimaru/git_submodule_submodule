abe57705651e55f22cdf65af5193bea57d7fabb2
Alberto Gutiérrez Torre <alberto.gutierrez@bsc.es>
2024-08-12 18:36:57 +0200

    fix: And now with proper spelling and coding.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 
 def print_number(int: num):
-    print(f"And here we have a number": num)
+    print(f"And here we have a number: {num}")
 
-def get_random_numer():
+def get_random_number():
     return(random.randint(0,9))
 
